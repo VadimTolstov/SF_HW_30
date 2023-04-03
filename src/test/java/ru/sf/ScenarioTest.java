bbb542f14602a9e51999478d8413bca169af7069
VadimTolstov <tolstov50@mail.ru>
2023-04-03 20:05:40 +0300

    HW 4

--- src/test/java/ru/sf/ScenarioTest.java
+++
@@ -1,4 +0,0 @@
-package ru.sf;
-
-public class ScenarioTest {
-}
---
+++ src/test/java/ru/sf/WeekAnalyser.java
@@ -0,0 +1,11 @@
+package ru.sf;
+
+public class WeekAnalyser {
+    public static String isItFridayToday(String today){
+        if (today.equals("Friday")){
+            return "Yes";
+        }else {
+            return "Nope";
+        }
+    }
+}
