3b1468bbc0e9f0ce756421764979e67727ceb5ad
VadimTolstov <tolstov50@mail.ru>
2023-04-04 20:38:58 +0300

    HW 4

--- src/test/java/ru/sf/StepDef.java
+++ src/test/java/ru/sf/StepDef.java
@@ -5,15 +5,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class StepDef {
+   // public static final  Get get;
+    private static final WebDriver webDriver;
 
-   private static final WebDriver webDriver;
-   //статический конструктор в котором мы поместим наш путь к веб драйверу
-   static {
-       System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-       webDriver = new ChromeDriver();
-   }
+    //статический конструктор в котором мы поместим наш путь к веб драйверу
+    static {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        webDriver = new ChromeDriver();
+    }
+
     @Given("url of h&m {string}")
     public void url_of_h_m(String url) {
-       webDriver.get(url);
+        webDriver.get(url);
     }
 }
